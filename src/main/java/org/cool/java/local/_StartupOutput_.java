f0ce9152d557fa55b92c5216b4802de3f58a15e3
Colin Davis <nilocsivad@aol.com>
2015-09-16 16:21:06 +0800

    output

--- src/main/java/org/cool/java/local/_StartupOutput_.java
+++ src/main/java/org/cool/java/local/_StartupOutput_.java
@@ -21,17 +21,6 @@ public class _StartupOutput_ {
 	 */
 	public static void main(String[] args) {
 		
-		for ( int i = 1001; i <= 1008; ++i ) {
-			System.out.println( "							<li><a href=\"#\"><img alt=\"\" src=\"\" data-src=\"./images/" + i + ".jpg\" /></a></li>" );
-//			System.out.println( "							<li><a href=\"#\" onclick=\"show_img(this)\" onmouseout=\"auto_show(true)\"></a></li>" );
-		}
-		
-		System.out.println( _StartupOutput_.class.getCanonicalName() + "-->" + _StartupOutput_.class.getSimpleName() );
-		
-		System.out.println( "=================================" );
-
-		boolean b = Boolean.parseBoolean( null );
-		System.out.println( b );
 	}
 
 }
