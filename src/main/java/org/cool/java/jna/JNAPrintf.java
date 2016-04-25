32b112552ec039eb44eb480c840f2fb4b007d921
Colin Davis <nilocsivad@aol.com>
2016-04-25 16:19:56 +0800

    jna

---
+++ src/main/java/org/cool/java/jna/JNAPrintf.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.cool.java.jna;
+
+import com.sun.jna.Library;
+
+/**
+ * @author Colin
+ * 
+ */
+public class JNAPrintf {
+
+	public interface CLibrary extends Library {
+
+	}
+
+	/**
+	 * 
+	 */
+	public JNAPrintf() {
+
+	}
+
+}
