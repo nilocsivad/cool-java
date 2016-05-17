143f6917708449c60348d08010a39e6923582095
Colin Davis <nilocsivad@aol.com>
2016-05-17 09:47:10 +0800

    move

--- src/main/java/org/cool_java/CmdMove2.java
+++ src/main/java/org/cool_java/CmdMove2.java
@@ -24,25 +24,67 @@ public class CmdMove2 {
 	 */
 	public static void main(String[] args) throws IOException {
 
-		String from = "F:\\2", dest = "F:\\3";
+		String from = "F:\\3", dest = "F:\\3";
 
 		File folder = new File(from);
 
-		list4(folder, dest, false);
+		String[] suffix = { ".bt.td", ".torrent" };
+		list4(folder, suffix);
+		move2(folder, dest, suffix, false);
 
 	}
 
-	static void list4(File folder, String dest, boolean createFolder) throws IOException {
+	static void list4(File folder, final String[] suffix) {
+		for (File f : folder.listFiles()) {
+			if (f.isDirectory()) {
+				list4(f, suffix);
+			} else {
+				if (suffix.length > 0) {
+					boolean _jmp = false;
+					for (String _suf : suffix) {
+						if (f.getName().endsWith(_suf)) {
+							_jmp = true;
+							break;
+						}
+					}
+					if (_jmp)
+						continue;
+				}
+				String p = f.getAbsolutePath();
+				System.out.println(p);
+			}
+		}
+	}
+
+	static void move2(File folder, String dest, final String[] suffix, boolean createFolder) throws IOException {
+
+		File _dest = new File(dest);
+		if (!_dest.exists() || _dest.isFile())
+			throw new RuntimeException(dest + " not exists or not a folder!");
+
 		for (File f : folder.listFiles()) {
 			if (f.isDirectory()) {
 				if (createFolder) {
 					File _f = new File(dest, f.getName());
 					_f.mkdirs();
-					list4(f, _f.getAbsolutePath(), createFolder);
+					move2(f, _f.getAbsolutePath(), suffix, createFolder);
 				} else {
-					list4(f, dest, createFolder);
+					move2(f, dest, suffix, createFolder);
 				}
 			} else {
+
+				if (suffix.length > 0) {
+					boolean _jmp = false;
+					for (String _suf : suffix) {
+						if (f.getName().endsWith(_suf)) {
+							_jmp = true;
+							break;
+						}
+					}
+					if (_jmp)
+						continue;
+				}
+
 				Runtime.getRuntime().exec(String.format(" cmd /c move \"%s\" \"%s\" ", f.getAbsolutePath(), dest));
 			}
 		}
